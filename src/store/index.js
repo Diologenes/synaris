47b6c1d882dd3753aafa21a43dacca4b33403e66
Gerald Jelitto <gerald.jelitto@googlemail.com>
2020-05-17 20:35:13 +0200

    prototyping sqlite

--- src/store/index.js
+++ src/store/index.js
@@ -2,18 +2,34 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import library from './modules/library'
 var sqlite3 = require('sqlite3').verbose()
+const fs = require('fs')
 
-const path = require('path')
-const dbPath = "./sqlite.db"
+const dbPath = './sqlite.db'
 
-console.log(path)
 console.log(dbPath)
-
-let db = new sqlite3.Database(dbPath, (err) => {
+ 
+fs.access(dbPath, fs.F_OK, (err) => {
 	if (err) {
-		console.error(err)
+		console.error(err.message)
+		let db = new sqlite3.Database(dbPath, (err) => {
+			const sqlTables = [
+				'CREATE TABLE "libraries" ("id"	INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,"uuid" TEXT UNIQUE,"group" INTEGER,"title"	TEXT,"sorting" INTEGER);',
+				'CREATE TABLE "article" ("id" INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, "uuid" TEXT UNIQUE, "title" TEXT, "content" TEXT, "labels" TEXT);'
+			]
+			sqlTables.forEach((value) => {
+				console.log(value)
+				db.run(value)
+			})
+			console.log('Database created')
+		})
 	} else {
-		console.log('Connected to the database.')
+		let db = new sqlite3.Database(dbPath, sqlite3.OPEN_READWRITE, (err) => {
+			if (err) {
+				console.error(err.message)
+			} else {
+				console.log('Connected to the chinook database.')
+			}
+		})
 	}
 })
 
