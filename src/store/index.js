4b60af6404885fb6ae4db63311dc95f5086060dd
Gerald Jelitto <gerald.jelitto@googlemail.com>
2020-05-17 14:40:13 +0200

    refactor

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import channel from './modules/channel'
+import library from './modules/library'
 
 // store
 Vue.use(Vuex)
 export const store = new Vuex.Store({
 	modules: {
-		channel
+		library
 	}
 })
