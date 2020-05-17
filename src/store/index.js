3e1785da05dce611813b0ab82a864a4222ad85df
Gerald Jelitto <gerald.jelitto@googlemail.com>
2020-05-17 16:19:06 +0200

    sqlite3 tests

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,21 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import library from './modules/library'
-var sqlite3 = require('sqlite3').verbose();
+var sqlite3 = require('sqlite3').verbose()
+
+const path = require('path')
+const dbPath = "./sqlite.db"
+
+console.log(path)
+console.log(dbPath)
+
+let db = new sqlite3.Database(dbPath, (err) => {
+	if (err) {
+		console.error(err)
+	} else {
+		console.log('Connected to the database.')
+	}
+})
 
 // store
 Vue.use(Vuex)
