cbd2ba9365e93cc065536ebb1ee145327400d95e
Gerald Jelitto <gerald.jelitto@googlemail.com>
2020-05-25 22:03:34 +0200

    own drag prototyping

--- src/routes/pages/Library.vue
+++ src/routes/pages/Library.vue
@@ -8,55 +8,65 @@
 				</div>
 				<!-- panel section -->
 
-				<!-- draggable -->
-				<draggable v-if="libraries !== null" v-model="libraries" ghostClass="c-library__item--ghost" touchStartThreshold="5" @start="startDrag()" @end="endDrag()">
-					<!-- group item -->
-					<div v-for="library in libraries" :key="library.id" class="c-library__group">
-						<!-- group title -->
-						<div class="c-library__group-title">{{ library.title }}</div>
-						<!-- group title -->
-
-						<!-- group content -->
-						<div class="c-library__group-content">
-							<b-link v-for="element in library.libraries" :key="element.id" router-tag="a" :to="{ name: 'library', params: { libraryId: element.id } }" class="c-library__item" active-class="active">
-								<div class="c-library__item-title" :title="element.title">{{ element.title }}</div>
-								<div class="c-library__item-indicator">
-									<div class="c-panelbox">
-										<div class="c-panelbox__item">0</div>
-									</div>
-								</div>
-							</b-link>
-						</div>
-						<!-- group item -->
+				<!-- group item -->
+				<div v-for="(library, libraryIndex) in libraries" :key="library.id" class="c-library__group">
+					<!-- group title -->
+					<div class="c-library__group-title" @dragstart="dragStart(libraryIndex, $event)" @dragover.prevent @dragenter="dragEnter(libraryIndex, $event)" @dragleave="dragLeave(libraryIndex, $event)" @dragend="dragEnd" @drop="dragFinish(libraryIndex, $event, libraryIndex)" draggable="true">
+						{{ library.title }}
 					</div>
-					<!-- group -->
-				</draggable>
-				<!-- draggable -->
+					<!-- group title -->
+
+					<!-- group content -->
+					<div class="c-library__group-content">
+						<b-link
+							@dragstart="dragStart(elementIndex, $event)"
+							@dragover.prevent
+							@dragenter="dragEnter(libraryIndex, $event)"
+							@dragleave="dragLeave(libraryIndex, $event)"
+							@dragend="dragEnd"
+							@drop="dragFinish(elementIndex, $event, libraryIndex)"
+							draggable="true"
+							v-for="(element, elementIndex) in library.libraries"
+							:key="element.id"
+							router-tag="a"
+							:to="{ name: 'library', params: { libraryId: element.id } }"
+							class="c-library__item"
+							active-class="active"
+						>
+							<div class="c-library__item-title" :title="element.title">{{ element.title }}</div>
+							<div class="c-library__item-indicator">
+								<div class="c-panelbox">
+									<div class="c-panelbox__item">0</div>
+								</div>
+							</div>
+						</b-link>
+					</div>
+					<!-- group item -->
+				</div>
+				<!-- group -->
 
 				<div v-b-modal.modal-create-new-library class="c-create-button c-button c-button--primary c-button--bullseye u-icon--more"></div>
 			</div>
 		</div>
 
 		<router-view />
-		<modal-add-library/>
-
+		<modal-add-library />
 	</div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
-import draggable from 'vuedraggable'
 import _ from 'lodash'
 import modalAddLibrary from '@/components/modals/AddLibrary'
 
 export default {
 	components: {
-		draggable,
 		modalAddLibrary
 	},
 	data() {
 		return {
-			isDragging: false
+			isDragging: false,
+			draggedId: null
 		}
 	},
 	computed: {
@@ -73,31 +83,34 @@ export default {
 		this.$store.dispatch('library/getAll')
 	},
 	methods: {
-		// create channel
-		createChannel() {
-			console.log('submit')
-			//Database.addLibrary()
+		changeDraggableItems(type) {
+			let items = window.document.getElementsByClassName('c-library__item')
+			items.forEach((element) => {
+				if (type === 'add') {
+					element.classList.add('c-library__item--is-drop')
+				} else if (type === 'remove') {
+					element.classList.remove('c-library__item--is-drop')
+				}
+			})
 		},
 
-		// start dragging
-		startDrag() {
-			this.isDragging = true
+		dragStart: function(i, event) {
+			console.log('dragStart', i, event)
+			this.changeDraggableItems('add')
+			this.draggedId = i
 		},
-
-		// sort channels by drag and drop
-		endDrag() {
-			let vm = this
-			vm.isDragging = false
-			if (vm.libraries.length) {
-				const idArray = []
-				_.forEach(vm.libraries, function(library) {
-					idArray.push(library.id)
-				})
-
-				// SQL HERE
-				//Database.updateLibrarySorting(idArray)
-				// SQL HERE
-			}
+		dragEnter: function(i, event) {
+			console.log('dragEnter', event)
+		},
+		dragLeave: function(i, event) {
+			// console.log('dragLeave', i, event)
+		},
+		dragEnd: function(event) {
+			this.changeDraggableItems('remove')
+		},
+		dragFinish: function(i, event, folderId) {
+			console.log('dragFinish / drop', this.draggedId, i, folderId)
+			console.log('dragFinish / drop ::', this.libraries)
 		}
 	}
 }
