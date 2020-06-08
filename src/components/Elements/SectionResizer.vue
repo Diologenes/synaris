2c14aa50d2b57606da2a7be8f785754f91aa0e98
Gerald Jelitto <gerald.jelitto@googlemail.com>
2020-06-08 21:28:39 +0200

    bugfix for sectionResizer

--- src/components/Elements/SectionResizer.vue
+++ src/components/Elements/SectionResizer.vue
@@ -1,108 +1,124 @@
 <template>
-	<div :class="styling" @mouseenter="enter()" @mouseleave="leave()" @mousedown="down()" @mouseup="up()" @mousemove="move($event)" v-click-outside="up"></div>
+	<div
+		:class="styling"
+		@mouseenter="enter()"
+		@mouseleave="leave()"
+		@mousedown="down($event)"
+		@mouseup="up()"
+		@mousemove="move($event)"
+		v-click-outside="up"
+	></div>
 </template>
 
 <script>
-import ClickOutside from 'vue-click-outside'
+	import ClickOutside from 'vue-click-outside'
 
-export default {
-	props: {
-		bindRef: {
-			type: String,
-			required: true
-		},
-		direction: {
-			type: String,
-			required: false,
-			default: 'horizontal'
-		},
-		defaultCssClass: {
-			type: String,
-			required: false,
-			default: 'c-resize'
-		},
-		min: {
-			type: Number,
-			default: 10,
-			required: false
-		},
-		max: {
-			type: Number,
-			default: 600,
-			required: false
-		},
-		dispatchToStore: {
-			type: String,
-			required: false
-		}
-	},
-	directives: {
-		ClickOutside
-	},
-	computed: {
-		styling() {
-			let classPrefix = this.defaultCssClass
-			let classes = []
-			classes.push(classPrefix)
-			classes.push(classPrefix + '--' + this.direction)
-			if (this.enabled) {
-				classes.push(classPrefix + '--is-active')
+	export default {
+		props: {
+			bindRef: {
+				type: String,
+				required: true
+			},
+			direction: {
+				type: String,
+				required: false,
+				default: 'horizontal'
+			},
+			defaultCssClass: {
+				type: String,
+				required: false,
+				default: 'c-resize'
+			},
+			min: {
+				type: Number,
+				default: 10,
+				required: false
+			},
+			max: {
+				type: Number,
+				default: 600,
+				required: false
+			},
+			dispatchToStore: {
+				type: String,
+				required: false
 			}
-			if (this.hover) {
-				classes.push(classPrefix + '--is-hover')
+		},
+		directives: {
+			ClickOutside
+		},
+		computed: {
+			styling() {
+				let classPrefix = this.defaultCssClass
+				let classes = []
+				classes.push(classPrefix)
+				classes.push(classPrefix + '--' + this.direction)
+				if (this.enabled) {
+					classes.push(classPrefix + '--is-active')
+				}
+				if (this.hover) {
+					classes.push(classPrefix + '--is-hover')
+				}
+				return classes.join(' ')
 			}
-			return classes.join(' ')
-		}
-	},
-	data() {
-		return {
-			enabled: false,
-			hover: false,
-			bindedElement: null,
-			calculatedDimension: '0px'
-		}
-	},
-	mounted() {
-		this.bindedElement = this.$parent.$refs[this.bindRef]
-	},
-	methods: {
-		enter() {
-			this.hover = true
 		},
-		leave() {
-			this.hover = false
+		data() {
+			return {
+				enabled: false,
+				hover: false,
+				bindedElement: null,
+				calculatedDimension: 0
+			}
 		},
-		down() {
-			this.enabled = true
+		mounted() {
+			this.bindedElement = this.$parent.$refs[this.bindRef]
 		},
-		up() {
-			if (this.enabled) {
+		methods: {
+			enter() {
+				this.hover = true
+			},
+			leave() {
 				this.hover = false
-				this.enabled = false
-				this.$emit('value', this.calculatedDimension)
-				if (typeof this.dispatchToStore !== 'undefined') {
-					this.$store.dispatch(this.dispatchToStore, this.calculatedDimension)
+			},
+			down($event) {
+				if ($event.which === 1) {
+					// only left mouse click allowed
+					this.enabled = true
 				}
-			}
-		},
-		move($event) {
-			if (this.enabled) {
-				if (this.direction === 'horizontal') {
-					this.calculatedDimension = this.calculateDimension($event.clientX, this.bindedElement.offsetLeft)
-					this.bindedElement.style.width = this.calculatedDimension
+			},
+			up() {
+				if (this.enabled && this.calculatedDimension >= this.min && this.calculatedDimension <= this.max) {
+					this.hover = false
+					this.enabled = false
+					this.$emit('value', this.pixelString(this.calculatedDimension))
+					if (typeof this.dispatchToStore !== 'undefined') {
+						this.$store.dispatch(this.dispatchToStore, this.pixelString(this.calculatedDimension))
+					}
 				}
+			},
+			move($event) {
+				if (this.enabled) {
+					if (this.direction === 'horizontal') {
+						console.log(this.bindedElement.offsetLeft)
+						this.calculatedDimension = this.calculateDimension($event.clientX, this.bindedElement.offsetLeft)
+						this.bindedElement.style.width = this.pixelString(this.calculatedDimension)
+					}
+				}
+			},
+			calculateDimension(currentPos, offset) {
+				let dimension = currentPos - offset
+				if (dimension <= this.min) {
+					dimension = this.min
+				}
+				if (dimension >= this.max) {
+					dimension = this.max
+				}
+				return dimension
+			},
+
+			pixelString(value) {
+				return `${value}px`
 			}
-		},
-		calculateDimension(currentPos, offset) {
-			let dimension = currentPos - offset
-			if (dimension <= this.min) {
-				dimension = this.min
-			}
-			if (dimension >= this.max) {
-				dimension = this.max
-			}
-			return dimension + 'px'
 		}
 	}
-}
 </script>
