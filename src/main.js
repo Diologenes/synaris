c2d0ccc68c5f690b9f86e513ecc853179f7b7d66
Gerald Jelitto <gerald.jelitto@googlemail.com>
2020-06-14 13:54:46 +0200

    keyboard shortcuts

--- src/main.js
+++ src/main.js
@@ -18,5 +18,26 @@ Vue.use(PerfectScrollbar)
 new Vue({
 	store,
 	router,
-	render: (h) => h(App)
+	methods: {
+		routeKeySwitch(route) {
+			if (this.$route.name !== route) this.$router.push({ name: route })
+		}
+	},
+	mounted() {
+		this._keyListener = function(e) {
+			if (e.key === '1' && (e.ctrlKey || e.metaKey)) {
+				e.preventDefault()
+				this.routeKeySwitch('categoryList')
+			}
+			if (e.key === '2' && (e.ctrlKey || e.metaKey)) {
+				e.preventDefault()
+				this.routeKeySwitch('search')
+			}
+		}
+		document.addEventListener('keydown', this._keyListener.bind(this))
+	},
+	beforeDestroy() {
+		document.removeEventListener('keydown', this._keyListener)
+	},
+	render: h => h(App)
 }).$mount('#app')
