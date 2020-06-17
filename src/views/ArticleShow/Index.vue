5db59bf41ef1156c0c589c8d434549226dfc2ea2
Gerald Jelitto <gerald.jelitto@googlemail.com>
2020-06-18 01:04:19 +0200

    quill prototyping

--- src/views/ArticleShow/Index.vue
+++ src/views/ArticleShow/Index.vue
@@ -1,15 +1,19 @@
 <template>
 	<div class="c-page c-page__default">
 		<div class="c-article-show">
-			Article here!
+			<div id="editor">{{ article.content }}</div>
 		</div>
 	</div>
 </template>
 
 <script>
+	import Quill from 'quill'
+
 	export default {
 		data() {
-			return {}
+			return {
+				editor: null
+			}
 		},
 		computed: {
 			category() {
@@ -28,11 +32,45 @@
 		},
 		mounted() {
 			this.getArticle()
+			this.initEditor()
 		},
 		methods: {
 			async getArticle() {
-				let vm = this
-				vm.$store.dispatch('article/setCurrentArticle', vm.$route.params.article)
+				await this.$store.dispatch('article/setCurrentArticle', this.$route.params.article)
+			},
+
+			initEditor() {
+				var toolbarOptions = [
+					['bold', 'italic', 'underline', 'strike'], // toggled buttons
+					['blockquote', 'code-block'],
+
+					[{ header: 1 }, { header: 2 }], // custom button values
+					[{ list: 'ordered' }, { list: 'bullet' }],
+					[{ script: 'sub' }, { script: 'super' }], // superscript/subscript
+					[{ indent: '-1' }, { indent: '+1' }], // outdent/indent
+					[{ direction: 'rtl' }], // text direction
+
+					[{ size: ['small', false, 'large', 'huge'] }], // custom dropdown
+					[{ header: [1, 2, 3, 4, 5, 6, false] }],
+
+					[{ color: [] }, { background: [] }], // dropdown with defaults from theme
+					[{ font: [] }],
+					[{ align: [] }],
+
+					['clean'] // remove formatting button
+				]
+
+				const options = {
+					debug: 'info',
+					modules: {
+						toolbar: toolbarOptions
+					},
+					placeholder: 'Compose an epic...',
+					readOnly: false,
+					theme: 'snow'
+				}
+
+				this.editor = new Quill(document.getElementById('editor'), options)
 			}
 		}
 	}
